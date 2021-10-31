c58bb2771ff39647ffcc685d0ada7c5338611657
André Maciel <andretmaciel@gmail.com>
2021-10-31 10:07:59 -0300

    publish app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fake KPI Tree
+
+This repo has a python classes and objects to generate a fake neural network, abstracting the idea of having 5 metric levels, with multiple KPI's that connects with each other, respecting the tiers, and random connections, all nodes has Parents and Childs randomized.
+
+![](../images/deep-neural-network.png)
---
+++ app/fake_kpi_tree_generator.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+
+
+class Metric:
+
+    def __init__(self, id):
+        self.id = id
+        self.name = 'metric name - ' + str(id)
+        self.parents = []
+        self.childs = []
+
+    def show_metric(self):
+        print(self.id)
+
+
+class Tree(Metric):
+
+    def __init__(self, rows, elements_size):
+        self.r = rows
+        self.elements_size = elements_size
+        self.rows = []
+        self.cells = []
+        self.all_paths = []
+
+    def generate_metric_rows(self):
+        for row in range(1, self.r+1):
+            self.rows.append(row)
+
+    def generate_random_metrics(self):
+        for row in self.rows:
+            for element in range(1, self.elements_size+1):
+                path_id = int(str(row) + str(element))
+                self.all_paths.append(Metric(id=path_id))
+                self.cells.append(path_id)
+
+    def randomize_network(self):
+        for path in self.all_paths:
+
+            # remove all non interested rows from the list used to build the random network
+            filtered_parents = list(filter(lambda cell: int(
+                str(cell)[0:1]) == int(str(path.id)[0:1])+1, self.cells))
+            filtered_childs = list(filter(lambda cell: int(
+                str(cell)[0:1]) == int(str(path.id)[0:1])-1, self.cells))
+
+            # define 2 parent
+            for i in range(2):
+                if filtered_parents:
+                    parent = np.random.choice(filtered_parents)
+                    path.parents.append(parent)
+                    filtered_parents.remove(parent)
+                    i += 1
+
+            # define 3 childs
+            for i in range(3):
+                if filtered_childs:
+                    child = np.random.choice(filtered_childs)
+                    path.childs.append(child)
+                    filtered_childs.remove(child)
+                    i += 1
+
+
+tree = Tree(5, 5)
+tree.generate_metric_rows()
+tree.generate_random_metrics()
+tree.randomize_network()
+
+print(tree)
